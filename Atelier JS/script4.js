22f5fefb8f8f68ebfc3194eb37a4b531281f071d
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-03-18 14:45:02 +0100

    corrected script3.1.js

--- Atelier JS/script4.js
+++ Atelier JS/script4.js
@@ -4,6 +4,6 @@
 function randomColor(){
     return Math.floor(Math.random()*16777215).toString(16);
 }
-/*Ce code marche pour la console mais pas come un fichier js*/
+
 document.querySelector("ol").addEventListener("click",
     (e)=>{e.target.style.color = "#"+randomColor().toUpperCase();});
