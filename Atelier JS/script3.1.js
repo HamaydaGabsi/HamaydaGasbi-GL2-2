06d5ac10285684e84e05e11fe404378f92385a5f
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-03-18 14:40:45 +0100

    corrected script3.1.js

--- Atelier JS/script3.1.js
+++ Atelier JS/script3.1.js
@@ -2,7 +2,8 @@
 /*les constantes suivantes reste toujours nuls*/
 const inputElements = document.querySelectorAll("input");
 const buttonElement = document.querySelector("button");
-
+const formElement=document.querySelector("form");
+formElement.addEventListener("input",()=>{handleState(inputElements,buttonElement)});
 function handleState(input,button){
     let empty=false;
     for(let i =0; i<input.length;i++){
@@ -13,6 +14,3 @@ function handleState(input,button){
     }
     button.disabled = empty;
 }
-/*J'ai pas trouver une solution avec eventListener qui marche mais
-* la fonction handleState marche convenablement*/
-document.querySelector("form").addEventListener("change", handleState(document.querySelectorAll("input"),document.querySelector("button")));
